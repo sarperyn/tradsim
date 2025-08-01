3babbb21adcaa3a9f813c8f7e06139accbc0c52c
sarperyn <sarperyn@gmail.com>
2025-08-01 18:47:10 +0300

    backtesting updated

--- frontend/views/backtesting.js
+++ frontend/views/backtesting.js
@@ -1,4 +1,7 @@
 import { fetchKlines } from "../services/binanceApi.js";
+import { runEMACrossStrategy } from "../strategies/emaCross.js";
+import { updateTradeTable } from "../utils/tradeTable.js";
+
 
 export function renderBacktesting(){
     const container = document.getElementById("app");
@@ -27,5 +30,43 @@ export function renderBacktesting(){
     </div>
 
     <div id="candle-container"></div>
+
+    <div id="trade-results">
+    <h3>Trade History</h3>
+    <table id="trade-table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Entry Time</th>
+          <th>Exit Time</th>
+          <th>Entry Price</th>
+          <th>Exit Price</th>
+          <th>Duration</th>
+          <th>P/L</th>
+          <th>P/L (%)</th>
+          <th>Type</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+    <div id="summary"></div>
+  </div>
   `;
+  document.getElementById("fetch-button").addEventListener("click", async () => {
+    const symbol = document.getElementById("symbol").value;
+    const interval = document.getElementById("interval").value;
+    const startStr = document.getElementById("start_str").value;
+    const endStr = document.getElementById("end_str").value;
+
+    try {
+      const data = await fetchKlines(symbol, interval, startStr, endStr);
+
+      const trades = runEMACrossStrategy(data);
+      updateTradeTable(trades);
+      console.log("Trades:", trades);
+
+    } catch (err) {
+      alert("Backtesting data fetch failed: " + err.message);
+    }
+});
 }
